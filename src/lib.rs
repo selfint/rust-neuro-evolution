cf73330746d56ad18d94243fa0ba1b5bcadbacd5
Tom Selfin <selfint@gmail.com>
2020-10-09 20:00:14 +0300

    use super instead of crate, I think this is better

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ mod neural_network;
 
 #[cfg(test)]
 mod tests {
-    use crate::neural_network::NeuralNetwork;
+    use super::neural_network::NeuralNetwork;
 
     #[test]
     fn neural_network_exists() {
