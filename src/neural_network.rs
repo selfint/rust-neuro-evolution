8ef7549a82e1850efc96e6e34ea5dc10aa35aee2
Tom Selfin <selfint@gmail.com>
2020-10-09 20:49:09 +0300

    add generate random weights

--- src/neural_network.rs
+++ src/neural_network.rs
@@ -1,11 +1,62 @@
+use rand::Rng;
+
 pub struct NeuralNetwork {
-    dims: Vec<usize>
+    dims: Vec<usize>,
+    weights: Vec<Vec<Vec<f32>>>
 }
 
 impl NeuralNetwork {
     pub fn new(dims: &Vec<usize>) -> NeuralNetwork {
         NeuralNetwork {
-            dims: dims.clone()
+            dims: dims.clone(),
+            weights: generate_random_weights(dims)
+        }
+    }
+}
+
+fn generate_random_weights(dims: &Vec<usize>) -> Vec<Vec<Vec<f32>>> {
+    let mut rng = rand::thread_rng();
+
+    let mut random_weights: Vec<Vec<Vec<f32>>> = vec![];
+
+    let next_dims = &dims[1..];
+    for (&prev_dim, &current_dim) in dims.iter().zip(next_dims.iter()) {
+        let mut layer_weights: Vec<Vec<f32>> = vec![];
+
+        for _ in 0..current_dim {
+            let mut node_weights: Vec<f32> = vec![];
+
+            for _ in 0..prev_dim {
+                node_weights.push(rng.gen_range(-0.1, 0.1));
+            }
+            layer_weights.push(node_weights);
+        }
+        random_weights.push(layer_weights);
+    }
+
+    random_weights
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn random_weights_have_correct_dims() {
+
+        // must have at least 2 layers
+        let dims = vec![2, 3, 1];
+        let random_weights = super::generate_random_weights(&dims);
+
+        for i in 1..dims.len() {
+            let current_dim = dims[i];
+            let prev_dim = dims[i-1];
+
+            let current_weights = &random_weights[i-1];
+
+            assert_eq!(current_dim, current_weights.len());
+
+            for node_weights in current_weights {
+                assert_eq!(prev_dim, node_weights.len());
+            }
         }
     }
 }
