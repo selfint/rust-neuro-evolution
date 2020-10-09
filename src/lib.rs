4dc9e293172b32260c415efb7908ae7058cd9865
Tom Selfin <selfint@gmail.com>
2020-10-09 20:49:38 +0300

    add submodule to integration tests for neural network tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,21 @@ mod neural_network;
 
 #[cfg(test)]
 mod tests {
-    use super::neural_network::NeuralNetwork;
+    mod neural_network_integration_tests {
+        use crate::neural_network::NeuralNetwork;
 
-    #[test]
-    fn neural_network_exists() {
-        let network_dims = vec![2, 3, 1];
-        let x = NeuralNetwork::new(&network_dims);
-    }
+        #[test]
+        fn neural_network_exists() {
+            let network_dims = vec![2, 3, 1];
+            let x = NeuralNetwork::new(&network_dims);
+        }
 
-    #[test]
-    fn constructor_doesnt_consume_dims() {
-        let network_dims = vec![2, 3, 1];
-        let x = NeuralNetwork::new(&network_dims);
-        let y = NeuralNetwork::new(&network_dims);
+        #[test]
+        fn constructor_doesnt_consume_dims() {
+            let network_dims = vec![2, 3, 1];
+            let x = NeuralNetwork::new(&network_dims);
+            let y = NeuralNetwork::new(&network_dims);
+        }
+
     }
 }
