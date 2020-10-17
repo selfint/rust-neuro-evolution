cc02faa74ac2a7c1c0fc8cfc0ced36a86836886b
Tom Selfin <selfint@gmail.com>
2020-10-17 21:36:44 +0300

    add board size test

--- src/environment.rs
+++ src/environment.rs
@@ -13,8 +13,23 @@ impl Bounce {
         Bounce {
             width,
             height,
-            board: vec![vec![BounceTile::Ground]],
+            board: Bounce::initialize_board(width, height),
         }
+    }
+
+    fn initialize_board(width: usize, height: usize) -> Vec<Vec<BounceTile>> {
+        let mut board = vec![];
+
+        for _ in 0..width {
+            let mut row = vec![];
+
+            for _ in 0..height {
+                row.push(BounceTile::Ground);
+            }
+            board.push(row);
+        }
+
+        board
     }
 }
 
@@ -30,6 +45,15 @@ mod tests {
 
         assert_eq!(board_width, b.width);
         assert_eq!(board_height, b.height);
+    }
+
+    #[test]
+    fn bounce_generates_correct_board_size() {
+        let board_width = 10;
+        let board_height = 30;
+        let b = Bounce::new(board_width, board_height);
+
+        assert_eq!(board_width, b.board.len());
     }
 
     #[test]
