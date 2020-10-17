31a0f1cd5cde7b0ab81b0cf6ee0c849b39c7b468
Tom Selfin <selfint@gmail.com>
2020-10-17 21:31:13 +0300

    add test for ground generation

--- src/environment.rs
+++ src/environment.rs
@@ -1,11 +1,20 @@
+enum BounceTile {
+    Ground,
+}
+
 pub struct Bounce {
     width: usize,
     height: usize,
+    board: Vec<Vec<BounceTile>>,
 }
 
 impl Bounce {
     pub fn new(width: usize, height: usize) -> Bounce {
-        Bounce { width, height }
+        Bounce {
+            width,
+            height,
+            board: vec![vec![BounceTile::Ground]],
+        }
     }
 }
 
@@ -21,5 +30,18 @@ mod tests {
 
         assert_eq!(board_width, b.width);
         assert_eq!(board_height, b.height);
+    }
+
+    #[test]
+    fn bounce_generates_ground() {
+        let board_width = 10;
+        let board_height = 30;
+        let b = Bounce::new(board_width, board_height);
+
+        for tile_row in b.board {
+            for tile in tile_row {
+                assert!(matches!(BounceTile::Ground, tile));
+            }
+        }
     }
 }
