305872c2e7d6f41a9146ec7c4ff16f21780f34e7
Tom Selfin <selfint@gmail.com>
2020-10-17 21:16:11 +0300

    refactor variable name

--- src/environment.rs
+++ src/environment.rs
@@ -17,9 +17,9 @@ mod tests {
     fn bounce_sets_correct_width_and_height() {
         let board_width = 10;
         let board_height = 30;
-        let b1 = Bounce::new(board_width, board_height);
+        let b = Bounce::new(board_width, board_height);
 
-        assert_eq!(board_width, b1.width);
-        assert_eq!(board_height, b1.height);
+        assert_eq!(board_width, b.width);
+        assert_eq!(board_height, b.height);
     }
 }
