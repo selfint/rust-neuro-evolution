8962c558d06a34fd349f16bb100582dea2033647
Tom Selfin <selfint@gmail.com>
2020-11-30 20:01:45 +0200

    rotate camera

--- src/startup.rs
+++ src/startup.rs
@@ -1,10 +1,11 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::camera::Camera};
 
 pub struct StartupPlugin;
 
 impl Plugin for StartupPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_startup_system(startup_system.system());
+        app.add_startup_system(startup_system.system())
+            .add_system(camera_rotater_system.system());
     }
 }
 
@@ -13,8 +14,8 @@ fn startup_system(mut commands: Commands) {
         // Camera
         .spawn(Camera3dComponents {
             transform: Transform::from_matrix(Mat4::from_rotation_translation(
-                Quat::from_xyzw(0., 90., 0., 0.).normalize(),
-                Vec3::new(0.0, 1.0, 0.0),
+                Quat::from_xyzw(0., -90., 0., 0.).normalize(),
+                Vec3::new(0.0, 1.0, -3.0),
             )),
             ..Default::default()
         })
@@ -23,4 +24,10 @@ fn startup_system(mut commands: Commands) {
             transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
             ..Default::default()
         });
+}
+
+fn camera_rotater_system(mut query: Query<(&Camera, &mut Transform)>) {
+    for (_camera, mut transform) in query.iter_mut() {
+        transform.rotate(Quat::from_rotation_y(0.01));
+    }
 }
