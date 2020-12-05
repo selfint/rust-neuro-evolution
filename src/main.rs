c11ee5bc9ae3168be82a29fef4a64fb8974fab7b
Tom Selfin <selfint@gmail.com>
2020-12-05 22:54:34 +0200

    add constatnts resource

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,12 @@ use creatures::CreaturesPlugin;
 use bevy::prelude::*;
 use bevy_rapier3d::physics::RapierPhysicsPlugin;
 
+struct Constants {
+    environment_size: f32,
+    initial_creatures: u32,
+    max_creatures: u32,
+}
+
 fn main() {
     App::build()
         .add_resource(Msaa { samples: 4 })
@@ -18,6 +24,11 @@ fn main() {
             width: 800,
             height: 800,
             ..Default::default()
+        })
+        .add_resource(Constants {
+            environment_size: 50.,
+            initial_creatures: 10,
+            max_creatures: 50,
         })
         .add_plugins(DefaultPlugins)
         .add_plugin(RapierPhysicsPlugin)
