dd73486989f48d53c7cd01bec9c5aa9524dbe301
Tom Selfin <selfint@gmail.com>
2020-12-06 10:35:50 +0200

    add walls surrounding environment

--- src/environment.rs
+++ src/environment.rs
@@ -1,9 +1,14 @@
 use bevy::prelude::*;
-use bevy_rapier3d::rapier::{dynamics::RigidBodyBuilder, geometry::ColliderBuilder};
+use bevy_rapier3d::rapier::{
+    dynamics::RigidBodyBuilder, geometry::ColliderBuilder, math::AngVector,
+};
+use std::f32::consts::PI;
 
 use crate::Constants;
 
-pub struct Ground;
+struct Ground;
+
+struct Wall;
 
 #[derive(Bundle)]
 struct GroundBundle {
@@ -14,8 +19,7 @@ pub struct EnvironmentPlugin;
 
 impl Plugin for EnvironmentPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_startup_system(environment_startup_system.system())
-            .add_system(clamp_system.system());
+        app.add_startup_system(environment_startup_system.system());
     }
 }
 
@@ -29,14 +33,91 @@ fn environment_startup_system(
         size: constants.environment_size * 2.,
     }));
     let ground_mat = materials.add(Color::rgb(0.1, 1., 0.).into());
+    let wall_mesh = meshes.add(Mesh::from(shape::Plane {
+        size: constants.environment_size * 2.,
+    }));
+    let wall_mat = materials.add(Color::rgba(0., 0., 0., 0.).into());
+    spawn_ground(
+        &mut commands,
+        constants.environment_size,
+        ground_mesh,
+        ground_mat,
+    );
 
+    // left
+    spawn_wall(
+        &mut commands,
+        constants.environment_size,
+        wall_mesh.clone(),
+        wall_mat.clone(),
+        AngVector::new(0., 0., PI / 2.),
+        constants.environment_size,
+        constants.environment_size,
+        0.,
+    );
+
+    // right
+    spawn_wall(
+        &mut commands,
+        constants.environment_size,
+        wall_mesh.clone(),
+        wall_mat.clone(),
+        AngVector::new(0., 0., PI / 2.),
+        -constants.environment_size,
+        constants.environment_size,
+        0.,
+    );
+
+    // front
+    spawn_wall(
+        &mut commands,
+        constants.environment_size,
+        wall_mesh.clone(),
+        wall_mat.clone(),
+        AngVector::new(PI / 2., 0., 0.),
+        0.,
+        constants.environment_size,
+        constants.environment_size,
+    );
+
+    // back
+    spawn_wall(
+        &mut commands,
+        constants.environment_size,
+        wall_mesh.clone(),
+        wall_mat.clone(),
+        AngVector::new(PI / 2., 0., 0.),
+        0.,
+        constants.environment_size,
+        -constants.environment_size,
+    );
+
+    // top
+    spawn_wall(
+        &mut commands,
+        constants.environment_size,
+        wall_mesh,
+        wall_mat,
+        AngVector::new(0., 0., 0.),
+        0.,
+        constants.environment_size * 2.,
+        0.,
+    );
+}
+
+fn spawn_ground(
+    commands: &mut Commands,
+    environment_size: f32,
+    ground_mesh: Handle<Mesh>,
+    ground_mat: Handle<StandardMaterial>,
+) {
     commands
         .spawn(GroundBundle { ground: Ground })
         .with(RigidBodyBuilder::new_static().translation(0.0, 0.0, 0.0))
         .with(ColliderBuilder::cuboid(
-            constants.environment_size,
+            environment_size,
             0.,
-            constants.environment_size,
+            environment_size,
         ))
         .with_bundle(PbrComponents {
             mesh: ground_mesh,
@@ -45,21 +126,32 @@ fn environment_startup_system(
         });
 }
 
-fn clamp_system(constants: Res<Constants>, mut query: Query<&mut Transform>) {
-    for mut transform in query.iter_mut() {
-        let entity_x = transform.translation.x();
-        let entity_y = transform.translation.y();
-        if entity_x.abs() > constants.environment_size {
-            match entity_x > 0. {
-                true => transform.translation.set_x(constants.environment_size),
-                false => transform.translation.set_x(-constants.environment_size),
-            }
-        }
-        if entity_y.abs() > constants.environment_size {
-            match entity_y > 0. {
-                true => transform.translation.set_y(constants.environment_size),
-                false => transform.translation.set_y(-constants.environment_size),
-            }
-        }
-    }
+// TODO: implement with less parameters?
+fn spawn_wall(
+    commands: &mut Commands,
+    environment_size: f32,
+    wall_mesh: Handle<Mesh>,
+    wall_mat: Handle<StandardMaterial>,
+    angle: AngVector<f32>,
+    x: f32,
+    y: f32,
+    z: f32,
+) {
+    commands
+        .spawn(PbrComponents {
+            mesh: wall_mesh,
+            material: wall_mat,
+            ..Default::default()
+        })
+        .with(Wall)
+        .with(
+            RigidBodyBuilder::new_static()
+                .rotation(angle)
+                .translation(x, y, z),
+        )
+        .with(ColliderBuilder::cuboid(
+            environment_size,
+            0.,
+            environment_size,
+        ));
 }
