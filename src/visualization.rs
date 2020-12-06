a582f86b2d9e59eb0bdbfaff600f858798b9129e
Tom Selfin <selfint@gmail.com>
2020-12-06 10:30:08 +0200

    add fly camera plugin

--- src/visualization.rs
+++ src/visualization.rs
@@ -1,13 +1,16 @@
 use bevy::prelude::*;
 use bevy::render::camera::Camera;
+use bevy_fly_camera::{FlyCamera, FlyCameraPlugin};
 
 pub struct VisualizationPlugin;
 
 impl Plugin for VisualizationPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_startup_system(visualization_startup_system.system())
+        app.add_plugin(FlyCameraPlugin)
+            .add_startup_system(visualization_startup_system.system())
             .add_system(camera_pointer_system.system())
-            .add_system(camera_rotator_system.system());
+            // .add_system(camera_rotator_system.system())
+            ;
     }
 }
 
@@ -15,9 +18,10 @@ fn visualization_startup_system(mut commands: Commands) {
     commands
         // Camera
         .spawn(Camera3dComponents {
-            transform: Transform::from_translation(Vec3::new(-50., 50., -50.)),
+            transform: Transform::from_translation(Vec3::new(-100., 75., -100.)),
             ..Default::default()
         })
+        .with(FlyCamera::default())
         // Light
         .spawn(LightComponents {
             transform: Transform::from_translation(Vec3::new(-40.0, 80.0, 30.0)),
@@ -28,19 +32,5 @@ fn visualization_startup_system(mut commands: Commands) {
 fn camera_pointer_system(mut query: Query<(&Camera, &mut Transform)>) {
     for (_c, mut transform) in query.iter_mut() {
         transform.look_at(Vec3::zero(), Vec3::unit_y());
-    }
-}
-
-fn camera_rotator_system(time: Res<Time>, mut query: Query<(&Camera, &mut Transform)>) {
-    // TOOD: there must be a better way to implement
-    for (_camera, mut transform) in query.iter_mut() {
-        let initial_y = transform.translation.y();
-        let left = transform
-            .forward()
-            .normalize()
-            .cross(Vec3::unit_y())
-            .normalize();
-        transform.translation += left * time.delta_seconds * 5.;
-        transform.translation.set_y(initial_y);
     }
 }
